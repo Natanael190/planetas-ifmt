a1b9682696dc31a59c2c5843d4c3d2227876224b
Natanael190 <naelpassos1@gmail.com>
2024-06-02 18:02:43 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Planetas do Sistema Solar</h1>
+    <PlanetList />
+  </div>
+</template>
+
+<script>
+import PlanetList from "./components/PlanetList.vue";
+
+export default {
+  name: "App",
+  components: {
+    PlanetList,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+  background-color: #1E0ED3;
+}
+
+h1,
+h2 {
+  color: #333;
+}
+</style>
